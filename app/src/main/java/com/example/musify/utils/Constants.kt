0169a59857caae5c6fa2dad15be728f43c533deb
Ash564738 <kieutrang5647@gmail.com>
2025-05-08 19:13:51 +0700

    fix push error

--- app/src/main/java/com/example/musify/utils/Constants.kt
+++ app/src/main/java/com/example/musify/utils/Constants.kt
@@ -1,9 +1,13 @@
 package com.example.musify.utils
 
+import com.example.musify.BuildConfig
+
 object Constants {
     const val CLIENT_ID = "4f4df92ddbae44738415fae5d6edc7b0"
     const val CLIENT_SECRET = "df2fdb0697714814ac715a5d908b355f"
     const val REDIRECT_URI = "musify://callback"
-    const val JAMENDO_CLIENT_ID = "d434f10c"
-    const val JAMENDO_CLIENT_SECRET = "e708d0f38b48ce162fdceedf01880b8f"
+    const val JAMENDO_CLIENT_ID = BuildConfig.JAMENDO_CLIENT_ID
+    const val JAMENDO_CLIENT_SECRET = BuildConfig.JAMENDO_CLIENT_SECRET
+
+
 }
